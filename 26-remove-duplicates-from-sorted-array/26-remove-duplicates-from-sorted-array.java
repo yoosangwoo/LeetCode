ecde31eb7d442f2e170de3342915da384599d863
yoosangwoo <82457713+yoosangwoo@users.noreply.github.com>
2022-08-23 17:25:47 +0900

    Time: 1 ms (100.00%), Space: 48.5 MB (7.74%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
@@ -8,7 +8,7 @@ class Solution {
         for(int j=1; j<nums.length; j++){
 //     2-1. nums[i]가 nums[j]와 같지않으면
             if( nums[i] != nums[j] ){
-//         2-1-1. i를 1 증가하고
+//         2-1-1. 개수를 셀 i에 1 증가하고
                 i++;                
 //         2-1-2. nums[i]에 nums[j]값을 대입
                 nums[i] = nums[j];
